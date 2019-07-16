3fb40545cc9d6c75db2c08dbe22657c85273a041
tyfal <tfalcoff@gmail.com>
2019-07-15 19:53:03 -0500

    providing option to get more (loads 10 more asyncronously on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,8 @@ class gifs {
 
     constructor() {
 
+        this.offset = 0;
+
         this.gifs = ["bear", "moose", "capybara"];
 
         this.renderButtons();
@@ -50,7 +52,9 @@ class gifs {
 
         $(".gif-btn").on("click", function () {
 
-            var urlQuery = "http://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "&api_key=JiBoAwx0n2p0NtUEJSBNedgrJrtsGLZU&limit=10&rating=PG";
+            var urlQuery = "http://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "&api_key=JiBoAwx0n2p0NtUEJSBNedgrJrtsGLZU&limit=10&rating=PG&offset=" + _self.offset;
+
+            var button = this;
 
             $.ajax({
                 url: urlQuery,
@@ -92,7 +96,71 @@ class gifs {
                     $("#gifs").append(gifDiv);
 
                 }
+
+                $("#get-more-div").append("<button id='get-more-btn'>Get mo'?</button>");
+
+                var _selfception = _self;
+
+                var _button = button;
+
+                $("#get-more-btn").on("click", function () {
+                    _selfception.getMore(_button);
+                });
+
             });
+
+        });
+
+    }
+
+    getMore(button) {
+
+        this.offset += 10;
+
+        console.log(this.offset);
+
+        var urlQuery = "http://api.giphy.com/v1/gifs/search?q=" + $(button).text() + "&api_key=JiBoAwx0n2p0NtUEJSBNedgrJrtsGLZU&limit=10&rating=PG&offset=" + this.offset;
+
+        $.ajax({
+            url: urlQuery,
+            method: "GET"
+        }).then(function (response) {
+
+            console.log(response);
+
+            for (var i = 0; i < response.data.length; i++) {
+
+                var gifDiv = $("<div>");
+
+                gifDiv.attr("class", "gif-div");
+
+                var gifImg = $("<img>");
+
+                gifImg.attr("class", "gif");
+
+                gifImg.attr("src", response.data[i].images.fixed_height_still.url);
+
+                gifImg.attr("hoversrc", response.data[i].images.fixed_height.url);
+
+                gifImg.mouseenter(function () {
+                    var tempSrc = $(this).attr("src");
+                    $(this).attr("src", $(this).attr("hoversrc"));
+                    $(this).attr("hoversrc", tempSrc);
+                    console.log("hover");
+                }).mouseleave(function () {
+                    var tempSrc = $(this).attr("src");
+                    $(this).attr("src", $(this).attr("hoversrc"));
+                    $(this).attr("hoversrc", tempSrc);
+                    console.log("leave");
+                });
+
+                gifDiv.append(gifImg);
+
+                gifDiv.append("<p>Rating: " + response.data[i].rating + "</p>");
+
+                $("#gifs").append(gifDiv);
+
+            }
 
         });
 
